75832680313a1bdcf691e331d5b731d65544d5eb
Evgen1ya <srtik93@gmail.com>
2022-07-31 13:06:45 +0700

    module, package, import

---
+++ main.py
@@ -0,0 +1,8 @@
+from data.people import get_employees
+from data.salary import calculate_salary
+import datetime
+
+if __name__ == '__main__':
+    get_employees()
+    calculate_salary()
+    datetime.today()
